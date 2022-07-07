b526ca8a74887a67b3f649cd35bfd9016a5adb41
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-07-07 16:00:22 +0200

    fine esercizio

--- film.php
+++ film.php
@@ -6,6 +6,8 @@ class Film{
     public $regista;
     public $anno;
 
+    public $Age;
+
     static public $num_of_film;
 
     public function __construct(string $nome_film, string $genere_film, string $regista_film, int $anno_film)
@@ -14,8 +16,9 @@ class Film{
         $this->genere = $genere_film;
         $this->regista = $regista_film;
         $this->anno = $anno_film;
-        self::$num_of_film++;
         $this->OldAge();
+        self::$num_of_film++;
+        
         
     }
 
@@ -23,12 +26,23 @@ class Film{
         $now = date("Y");
         //se la sottrazzione di anno film e anno attuale e meno di 5 il film è 'recente'
         if ( $now - $this->anno < 5) {
-            echo "il film è recente";
+            $this->Age = "il film è recente";
             
         } else {
-            echo "il film è vecchio";
+            $this->Age = "il film è vecchio";
         }
         
+        
+    }
+
+    public function Print(){
+        echo '<div class="film">';
+        echo "<p> Nome :" . $this->nome . "</p>";
+        echo "<p> Genere :" . $this->genere . "</p>";
+        echo "<p> Regista :" .$this->regista . "</p>";
+        echo "<p> Anno :" . $this->anno . "</p>";
+        echo "<p>". $this->Age . "</p>";
+        echo "</div>";
         
     }
 }
--- index.php
+++ index.php
@@ -1,12 +1,28 @@
 <?php
-include __DIR__ .'/film.php';
+include_once __DIR__ .'/film.php';
 
 $top_gun = new Film('TopGun','Azione','Joseph Kosinski',2022);
-var_dump($top_gun);
 $nostalgia = new Film('nostalgia','Drammatico','Mario Martone',2010);
-var_dump($nostalgia);
 $diabolik = new Film('diabolik','Giallo',' Antonio Manetti',2021);
-var_dump($diabolik);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Film</title>
+</head>
+<body>
+    <div class="container"><?php
+      echo $top_gun->Print() . $nostalgia->Print() . $diabolik->Print() ;
+
+    ?></div>
+</body>
+</html>
 
 
 
