e42137b0aedacbb6b1feefbd4d9c839a63fa9c7c
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-07-07 15:14:45 +0200

    primo metodo inserito

--- film.php
+++ film.php
@@ -14,6 +14,21 @@ class Film{
         $this->genere = $genere_film;
         $this->regista = $regista_film;
         $this->anno = $anno_film;
+        self::$num_of_film++;
+        $this->OldAge();
+        
+    }
+
+    public function OldAge() {
+        $now = date("Y");
+        //se la sottrazzione di anno film e anno attuale e meno di 5 il film è 'recente'
+        if ( $now - $this->anno < 5) {
+            echo "il film è recente";
+            
+        } else {
+            echo "il film è vecchio";
+        }
+        
         
     }
 }
--- index.php
+++ index.php
@@ -3,8 +3,10 @@ include __DIR__ .'/film.php';
 
 $top_gun = new Film('TopGun','Azione','Joseph Kosinski',2022);
 var_dump($top_gun);
-$nostalgia = new Film('nostalgia','Drammatico','Mario Martone',2022);
+$nostalgia = new Film('nostalgia','Drammatico','Mario Martone',2010);
 var_dump($nostalgia);
 $diabolik = new Film('diabolik','Giallo',' Antonio Manetti',2021);
 var_dump($diabolik);
+
+
 
